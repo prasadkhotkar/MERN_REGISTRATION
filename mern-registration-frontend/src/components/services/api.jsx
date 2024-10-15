c2ceaeac6387be3760556f395352194cc4328cff
Prasad khotkar <109893694+prasadkhotkar@users.noreply.github.com>
2024-10-15 15:27:17 +0530

    Update api.jsx

--- mern-registration-frontend/src/components/services/api.jsx
+++ mern-registration-frontend/src/components/services/api.jsx
@@ -1,11 +1,10 @@
-// src/services/api.js
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend URL
+  baseURL: 'http://localhost:5000/api',
 });
 
-// Add a request interceptor to include the token in headers
+
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
